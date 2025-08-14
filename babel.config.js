85e79c8105d305678501daa31e3ce3c0d723f007
dhzmqjhf@gmail.com <dhzmqjhf@gmail.com>
2025-08-14 22:02:39 +0900

    바벨 설정 및 타입스크립트 컴파일러 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@': './src',
+          '@app': './src/app',
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@features': './src/features',
+          '@navigation': './src/navigation',
+          '@assets': './src/assets',
+          '@hooks': './src/hooks',
+          '@utils': './src/utils',
+          '@store': './src/store',
+          '@types': './src/types',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
