a956d53e7f6fcc9ca3df8bf9c49798be7671571a
dhzmqjhf@gmail.com <dhzmqjhf@gmail.com>
2025-08-17 18:30:41 +0900

    Reanimated 런타임/워크릿 초기화

--- App.tsx
+++ App.tsx
@@ -1,17 +1,21 @@
 import 'react-native-gesture-handler';
+import 'react-native-reanimated';
 import RootTabNavigator from '@/navigation/RootTabNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <StatusBar barStyle="dark-content" />
-        <RootTabNavigator />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <GestureHandlerRootView>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <StatusBar barStyle="dark-content" />
+          <RootTabNavigator />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 
